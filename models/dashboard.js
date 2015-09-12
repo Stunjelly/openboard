d872e58a6c717ed2f2455eec5be611fc7dc34689
Edward Knowles <mail@eknowles.com>
2015-09-12 21:46:04 +0100

    tidy models

--- models/dashboard.js
+++ models/dashboard.js
@@ -1,26 +1,10 @@
 module.exports = function (sequelize, DataTypes) {
+
   var Dashboard = sequelize.define('Dashboard', {
-    userId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      len: [0, 40]
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      len: [0, 40]
-    },
-    'public': {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    columns: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 3,
-      len: [3, 6]
-    }
+    userId: {type: DataTypes.STRING, allowNull: false, len: [0, 40]},
+    title: {type: DataTypes.STRING, allowNull: false, len: [0, 40]},
+    'public': {type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false},
+    columns: {type: DataTypes.INTEGER, allowNull: false, defaultValue: 3, len: [3, 6]}
   });
 
   return Dashboard
--- models/widget.js
+++ models/widget.js
@@ -1,68 +1,15 @@
-module.exports = function(sequelize, DataTypes) {
-  var Widget = sequelize.define('widget', {
-    dashboardId: {
-      type: DataTypes.INTEGER,
-      validate: {
-        notNull: true
-      }
-    },
-    title: {
-      type: DataTypes.STRING,
-      validate: {
-        notNull: false,
-        len: [0, 100]
-      }
-    },
-    width: {
-      type: DataTypes.INTEGER,
-      validate: {
-        notNull: true,
-        min: 1,
-        max: 6
-      }
-    },
-    height: {
-      type: DataTypes.INTEGER,
-      validate: {
-        notNull: true,
-        min: 1,
-        max: 6
-      }
-    },
-    apiKey: {
-      type: DataTypes.STRING,
-      validate: {
-        notNull: true,
-        len: [0, 36]
-      }
-    },
-    method: {
-      type: DataTypes.ENUM('push', 'polling'),
-      validate: {
-        notNull: true
-      }
-    },
-    type: {
-      type: DataTypes.INTEGER,
-      validate: {
-        notNull: true,
-        min: 1,
-        max: 20
-      }
-    },
-    url: {
-      type: DataTypes.STRING,
-      validate: {
-        notNull: false
-      }
-    },
-    urlKey: {
-      type: DataTypes.STRING,
-      validate: {
-        notNull: true,
-        len: [0, 36]
-      }
-    }
+module.exports = function (sequelize, DataTypes) {
+
+  var Widget = sequelize.define('Widget', {
+    dashboardId: {type: DataTypes.INTEGER, allowNull: false},
+    title: {type: DataTypes.STRING, allowNull: false, len: [0, 100]},
+    width: {type: DataTypes.INTEGER, allowNull: false, min: 1, max: 6},
+    height: {type: DataTypes.INTEGER, allowNull: false, min: 1, max: 6},
+    apiKey: {type: DataTypes.STRING, allowNull: false, len: [0, 36]},
+    method: {type: DataTypes.ENUM('push', 'polling'), allowNull: false},
+    type: {type: DataTypes.INTEGER, allowNull: false, min: 1, max: 20},
+    url: {type: DataTypes.STRING, allowNull: true},
+    urlKey: {type: DataTypes.STRING, allowNull: true, len: [0, 36]}
   });
 
   return Widget
