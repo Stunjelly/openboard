ae3ce7e54d76c64ebb3bb8e0949eede9ce55540b
Edward Knowles <mail@eknowles.com>
2015-09-13 14:53:13 +0100

    add x y pos to widgets and add grid list lib

--- models/widget.js
+++ models/widget.js
@@ -3,8 +3,10 @@ module.exports = function (sequelize, DataTypes) {
   var Widget = sequelize.define('Widget', {
     dashboardId: {type: DataTypes.INTEGER, allowNull: false},
     title: {type: DataTypes.STRING, allowNull: false, len: [0, 100]},
-    width: {type: DataTypes.INTEGER, allowNull: false, min: 1, max: 6},
-    height: {type: DataTypes.INTEGER, allowNull: false, min: 1, max: 6},
+    w: {type: DataTypes.INTEGER, allowNull: false, min: 1, max: 6},
+    h: {type: DataTypes.INTEGER, allowNull: false, min: 1, max: 6},
+    x: {type: DataTypes.INTEGER, allowNull: false, min: 0, max: 6},
+    y: {type: DataTypes.INTEGER, allowNull: false, min: 0, max: 6},
     apiKey: {type: DataTypes.STRING, allowNull: false, len: [0, 36]},
     method: {type: DataTypes.ENUM('push', 'polling'), allowNull: false},
     type: {type: DataTypes.INTEGER, allowNull: false, min: 1, max: 20},
