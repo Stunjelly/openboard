8d259559a94619148ec535b40e449f40b417e57e
Edward Knowles <mail@eknowles.com>
2015-09-14 11:18:06 +0100

    add edit dashboard modal

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,13 @@ exports = module.exports = function (app) {
   app.get('/api/dashboards', dashboards.findAll);
   app.get('/api/dashboards/:dashboardId', dashboards.find);
   app.post('/api/dashboards', dashboards.create);
-  app.put('/api/dashboards/:dashboardId', dashboards.update);
+  app.post('/api/dashboards/:dashboardId', dashboards.update);
   app.del('/api/dashboards/:dashboardId', dashboards.destroy);
 
   app.get('/api/dashboards/:dashboardId/widgets', widgets.findAll);
   app.get('/api/dashboards/:dashboardId/widgets/:widgetId', widgets.find);
   app.post('/api/dashboards/:dashboardId/widgets', widgets.create);
-  app.put('/api/dashboards/:dashboardId/widgets/:widgetId', widgets.update);
+  app.post('/api/dashboards/:dashboardId/widgets/:widgetId', widgets.update);
   app.del('/api/dashboards/:dashboardId/widgets/:widgetId', widgets.destroy);
 
   app.get('/api/me', function (req, res) {
