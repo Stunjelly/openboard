d799ce85e953f29850e6a8b65161b1a606cda678
Edward Knowles <mail@eknowles.com>
2015-09-20 18:46:55 +0100

    add first widget directive number and secondary

--- public/directive/sparkline/sparkline.js
+++ public/directive/sparkline/sparkline.js
@@ -2,42 +2,28 @@ angular.module('openboard').directive('sparkline', function () {
   return {
     restrict: 'E',
     replace: true,
-    transclude: true,
-    scope: {
-      data: '=data'
-    },
     templateUrl: 'directive/sparkline/sparkline.html',
     link: function (scope, el, attr, fn) {
-      if (scope.data) {
-        var svg;
-        var x;
-        var y;
-        var h;
-        var w;
-        var m;
-        var r;
-        var max;
-        var min;
-        var data;
-        el = d3.select(el[0]);
-        svg = el;
-        data = scope.data;
+      function drawSVG(d) {
+        var svg, x, y, h, w, m, r, max, min, data;
+        svg = d3.select(el[0]);
+        data = d;
         min = attr.min !== undefined ? +attr.min : d3.min(data);
         max = attr.max !== undefined ? +attr.max : d3.max(data);
         r = attr.r || 0;
         m = r;
-        //var w = svg.node().clientWidth;
-        //var h = +getComputedStyle(el.node())['font-size'].replace('px', '');
         w = 160;
         h = 80;
-        svg.attr({width: w, height: h});
+
+        svg.selectAll("*").remove();
+
+        svg.attr('width', w).attr('height', h);
 
         x = d3.scale.linear().domain([0, data.length - 1]).range([m, w - m]);
         y = d3.scale.linear().domain([min, max]).range([h - m, m]);
 
         // Draw Sparkline Path
-        svg
-          .append('path')
+        svg.append('path')
           .data(data)
           .attr('d', 'M' + data.map(function (d, i) {
             return [x(i), y(d)]
@@ -50,10 +36,14 @@ angular.module('openboard').directive('sparkline', function () {
           .attr('width', 60)
           .attr('stroke', 0)
           .text(function () {
-            var d = data.reverse();
+            var d = data;
+            //d.reverse();
             return d[0];
           });
       }
+      scope.$watchCollection('widget.cache.item[1]', function (newVal, oldVal) {
+        return drawSVG(scope.widget.cache.item[1]);
+      });
     }
   };
 });
