90d1ae1f5488b656dc8e3f8bc7e31e74a91988f7
Edward Knowles <mail@eknowles.com>
2015-09-21 18:02:22 +0100

    clean up types API

--- routes/types.js
+++ routes/types.js
@@ -2,46 +2,32 @@ var db = require('../models');
 
 exports.findAll = function (req, res) {
   db.Type.findAll().then(function (entities) {
-    res.json(entities)
+    return res.json(entities);
   }, function (err) {
-    res.send(400, err);
+    return res.send(400, err);
   })
 };
 
 exports.find = function (req, res) {
-  var query = {
-    where: {
-      id: req.param('typeId')
-    }
-  };
-  if (req.param('field')) {
-    query.attributes = req.param('field').split(',');
-  }
-  db.Type.find(query).then(function (entity) {
-    if (entity) {
-      res.json(entity)
-    } else {
-      res.send(404)
-    }
+  var q = req.param('field') ? db.Type.find({
+    where: {id: req.param('typeId')},
+    attributes: req.param('field').split(',')
+  }) : db.Type.findById(req.param('typeId'));
+  q.then(function (entity) {
+    return entity ? res.json(entity) : res.send(404);
   }, function (err) {
-    res.send(400, err);
+    return res.send(400, err);
   })
 };
 
+// TODO: limit this to an admin :)
 exports.update = function (req, res) {
-  db.Type.find({
-    where: {
-      id: req.param('typeId')
-    }
-  }).then(function (entity) {
-    if (entity) {
-      entity.updateAttributes(req.body).then(function (entity) {
-        res.json(entity)
-      })
-    } else {
-      res.send(404)
-    }
+  db.Type.findById(req.param('typeId')).then(function (entity) {
+    if (!entity) return res.send(404);
+    entity.updateAttributes(req.body).then(function (entity) {
+      return res.json(entity);
+    });
   }, function (err) {
-    res.send(400, err);
+    return res.send(400, err);
   })
 };
