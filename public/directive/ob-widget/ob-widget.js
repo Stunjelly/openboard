2742a8d51181f2a28ec6fedbe32b26ec51f6ef0b
Edward Knowles <mail@eknowles.com>
2015-09-24 11:40:34 +0100

    tidy ob-widget directive js

--- public/directive/ob-widget/ob-widget.js
+++ public/directive/ob-widget/ob-widget.js
@@ -5,15 +5,18 @@ angular.module('openboard').directive('obWidget', function ($modal, $compile, $i
     templateUrl: 'directive/ob-widget/ob-widget.html',
     link: function (scope, element, attrs, fn) {
 
-      var widgetHolder = element.find('.widget-directive-holder');
-      widgetHolder.attr('widget-' + scope.widget.typeId, '');
+      // Compile and load the widget type directive
+      var widgetHolder = element
+        .find('.widget-directive-holder')
+        .attr('widget-' + scope.widget.typeId, '');
+
       $compile(widgetHolder)(scope);
 
+      // On widget load ask for the latest data
+      SocketService.emit('request:cache', {widgetId: scope.widget.id});
+
+      // Emit a socket at a designated interval
       $interval(function () {
-        //var n = Math.floor(Math.random() * 300) + 100;
-        //scope.widget.cache.item[0].value += n;
-        //scope.widget.cache.item[1].push(n);
-        //scope.widget.cache.item[1].splice(0, 1);
         SocketService.emit('request:cache', {widgetId: scope.widget.id});
       }, scope.widget.reload * 1000);
 
@@ -25,35 +28,35 @@ angular.module('openboard').directive('obWidget', function ($modal, $compile, $i
       // Setup init position
       element[0].style.transform = 'translate(' + scope.widget.x + 'px, ' + scope.widget.y + 'px)';
 
+      // On mouse move update widget scope with x/y and move it with webkit transform
+      function dragMove(event) {
+        scope.widget.x += event.dx;
+        scope.widget.y += event.dy;
+        event.target.style.webkitTransform =
+          event.target.style.transform = 'translate(' + scope.widget.x + 'px, ' + scope.widget.y + 'px)';
+      }
+
+      // On mouse up dragging widget update position
+      function dragEnd(e) {
+        scope.widget.$save();
+      }
+
       // Init interact on the element
-      interact(element[0])
-        .draggable({
-          snap: {
-            targets: [
-              interact.createSnapGrid({x: 10, y: 10})
-            ],
-            range: Infinity,
-            relativePoints: [{x: 0, y: 0}]
-          },
-          inertia: false,
-          restrict: {
-            restriction: element.parentNode,
-            elementRect: {top: 0, left: 0, bottom: 1, right: 1},
-            endOnly: true
-          }
-        })
-        .on('dragmove', function (event) {
-
-          scope.widget.x += event.dx;
-          scope.widget.y += event.dy;
-
-
-          event.target.style.webkitTransform =
-            event.target.style.transform = 'translate(' + scope.widget.x + 'px, ' + scope.widget.y + 'px)';
-        })
-        .on('dragend', function (event) {
-          scope.widget.$save();
-        });
+      interact(element[0]).draggable({
+        snap: {
+          targets: [interact.createSnapGrid({x: 10, y: 10})],
+          range: Infinity,
+          relativePoints: [{x: 0, y: 0}]
+        },
+        inertia: false,
+        restrict: {
+          restriction: element.parentNode,
+          elementRect: {top: 0, left: 0, bottom: 1, right: 1},
+          endOnly: true
+        }
+      })
+        .on('dragmove', dragMove)
+        .on('dragend', dragEnd);
     }
   };
 });
